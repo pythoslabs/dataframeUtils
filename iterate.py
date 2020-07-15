c1207581fd265673bdfb32a2072294d84957beb4
Pythos Labs <48703756+pythoslabs@users.noreply.github.com>
2020-07-15 18:23:37 +0530

    Update iterate.py
    error in syntax

--- iterate.py
+++ iterate.py
@@ -3,8 +3,8 @@ f_scrip = pd.read_csv('file')
 # Columnns in this dataframe are 'symbol,open, high, low, close'
 
 for index, row in f_scrip.iterrows():
-  high = row('high')
-  symbol,high = row['symbol'],row['high']
+  high = row['high']  # To assign only one value
+  symbol,high = row['symbol'],row['high'] # To assign multiple values from columns
 
 
 
